4f34c3ab1b460b0ebdf9256d89707da0f984dff3
Luca Lowndes <104835046+LucaLow@users.noreply.github.com>
2025-01-29 14:45:02 +1100

    Remove example script and unnecessary requirements; update package structure and metadata for improved organization and compatibility, along with publishing to test.pypi.org

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='mnist_interactive',
-    version='1.0.0',
+    version='1.0.4',
     description='A library for experimenting with MNIST models using an interactive 28x28 grid.',
     author='Luca Lowndes',
     author_email='Luca@Lowndes.net',
     url='https://github.com/LucaLow/MNIST-Interactive-Model-Analyzer',
-    packages=find_packages(),
+    package_dir={"": "src"},
+    packages=find_packages(where='src'),
     install_requires=[
         'tensorflow>=2.0',
         'numpy',
