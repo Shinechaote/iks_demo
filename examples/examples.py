f52f287bff8464744709cec03a177a513985a641
Luca Lowndes <104835046+LucaLow@users.noreply.github.com>
2025-01-29 14:50:39 +1100

    Add example script for using mnist-interactive-model-analyzer, aswell as a sample model

---
+++ examples/examples.py
@@ -0,0 +1,18 @@
+import mnist_interactive.numberCreatorWindow as mi
+import numpy as np
+import tensorflow as tf
+import tkinter as tk
+from tkinter import ttk
+
+
+# Load your pre-trained MNIST model
+model = tf.keras.models.load_model('sampleModel.keras')
+
+# create the tkinter window
+root = tk.Tk()
+
+
+# Initialize the interactive grid
+grid = mi.NumberCreatorWindow(root, blur=0.15, model=model)
+
+root.mainloop()
